5524adb226306628233a9bdd7fc29b93890d0097
Diago <diagorr@gmail.com>
2026-04-18 16:23:38 -0300

    Use putenv() for robust Vercel vars

--- api/index.php
+++ api/index.php
@@ -1,9 +1,9 @@
 <?php
 
 // Vercel uses a read-only filesystem, so we MUST redirect Laravel storage/cache to /tmp.
-$_ENV['VIEW_COMPILED_PATH'] = '/tmp/views';
-$_ENV['SESSION_DRIVER'] = 'database';
-$_ENV['LOG_CHANNEL'] = 'stderr';
+putenv('VIEW_COMPILED_PATH=/tmp/views');
+putenv('SESSION_DRIVER=database');
+putenv('LOG_CHANNEL=stderr');
 
 if (!is_dir('/tmp/views')) {
     mkdir('/tmp/views', 0777, true);
